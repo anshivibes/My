ca1d28bca32f2d7942dc41d65bc56cd1891a7d59
anshivibes <anshikabijalwan2020@outlook.com>
2024-12-07 11:19:02 +0530

    initial commit

--- 1st.py
+++ 1st.py
@@ -1,45 +1,22 @@
-import hashlib
-print("**************PASSWORD CRACKER ******************")
-		
-# To check if the password
-# found or not.
-pass_found = 0									
+import socket
 
-input_hash = input("Enter the hashed password:")
+# Define server address and port
+HOST = '127.0.0.1'  # Localhost
+PORT = 65432        # The same port the server is listening on
 
-pass_doc = input("\nEnter passwords filename including path(root / home/):")
+# Create a TCP/IP socket
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-try:
-	# trying to open the password file.
-	pass_file = open(pass_doc, 'r')			 
-except:
-	print("Error:")
-	print(pass_doc, "is not found.\nPlease give the path of file correctly.") 
-	quit()
+# Connect to the server
+client_socket.connect((HOST, PORT))
 
+# Send some data to the server
+message = "Hello, server!"
+client_socket.sendall(message.encode())
 
-# comparing the input_hash with the hashes
-# of the words in password file,
-# and finding password.
+# Receive the response from the server
+data = client_socket.recv(1024)
+print(f"Received from server: {data.decode()}")
 
-for word in pass_file:
-	# encoding the word into utf-8 format
-	enc_word = word.encode('utf-8') 
-			
-	# Hashing a word into md5 hash
-	hash_word = hashlib.md5(enc_word.strip()) 
-
-	# digesting that hash into a hexa decimal value	 
-	digest = hash_word.hexdigest()		 
-	
-	if digest == input_hash:
-		# comparing hashes
-		print("Password found.\nThe password is:", word) 
-		pass_found = 1
-		break
-
-# if password is not found.
-if not pass_found:
-	print("Password is not found in the", pass_doc, "file") 
-	print('\n')
-print("***************** Thank you **********************")
+# Close the connection
+client_socket.close()
